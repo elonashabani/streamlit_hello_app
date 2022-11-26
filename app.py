b10ab9d57538ac542c27921c36f2dc2e4c1a9502
asd <asd@asd.asd>
2022-11-26 14:11:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 
 st.title("Hello Streamlit app")
-st.header("My name is Elona and this is my deployed app")
+st.header("My name is Elona and this is my first deployed app")
